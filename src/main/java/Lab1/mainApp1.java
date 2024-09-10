7360ca1889f40dd7b20f3e761a6eb88db7a5bee2
SW/Andy <li.ck8819@gmail.com>
2024-09-10 11:15:55 +0800

    Add 2 output in mainApp1.java

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Program ..Starting ...");
+        System.out.println("1 + 1 = " + 2);
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
